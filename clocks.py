10b9ba78b289368ed438e4d14a88736a08a92d6b
Jon Dawson <chips@jondawson.org.uk>
2024-08-11 13:18:10 +0100

    Add python script to calculate clock frequencies

--- clocks.py
+++ clocks.py
@@ -1,38 +1,40 @@
 from math import ceil, floor
+import numpy as np
 
 
-minimum_frequency = 125e6
-maximum_frequency = 133e6
-valid_system_clocks = {}
+def valid_system_clocks(minimum_frequency, maximum_frequency):
+  valid_system_clocks = {}
 
-#generate a table of all system frequencies in the valid range
-for refdiv in reversed(range(1, 2)):
-  reffreq = 12e6/refdiv
-  for fbdiv in range(16, 320):
-    vco_freq = reffreq*fbdiv
-    if vco_freq > 1600e6:
-      continue
-    if vco_freq < 750e6:
-      continue
-    for postdiv1 in range(1, 8):
-      for postdiv2 in range(1, 8):
-        f = vco_freq/(postdiv1*postdiv2)
-        if f > maximum_frequency:
-          continue
-        if f < minimum_frequency:
-          continue
-        valid_system_clocks[int(f)] = (refdiv, fbdiv, postdiv1, postdiv2)
+  #generate a table of all system frequencies in the valid range
+  for refdiv in reversed(range(1, 2)):
+    reffreq = 12e6/refdiv
+    for fbdiv in range(16, 320):
+      vco_freq = reffreq*fbdiv
+      if vco_freq > 1600e6:
+        continue
+      if vco_freq < 750e6:
+        continue
+      for postdiv1 in range(1, 8):
+        for postdiv2 in range(1, 8):
+          f = vco_freq/(postdiv1*postdiv2)
+          if f > maximum_frequency:
+            continue
+          if f < minimum_frequency:
+            continue
+          valid_system_clocks[int(f)] = (refdiv, fbdiv, postdiv1, postdiv2)
 
-print("valid_system_clocks")
-ratios = []
-for i, f in enumerate(sorted(valid_system_clocks.keys())):
-  settings = valid_system_clocks[f]
-  print("{%u, %u, %u, %u, %u},"%((f,)+settings))
-  ratios.append(f/minimum_frequency)
+  print("valid_system_clocks")
+  ratios = []
+  for i, f in enumerate(sorted(valid_system_clocks.keys())):
+    settings = valid_system_clocks[f]
+    print("{%u, %u, %u, %u, %u},"%((f,)+settings))
+    ratios.append(f/minimum_frequency)
 
-def best_clock_frequency(desired_frequency):
+  return valid_system_clocks
+
+def best_clock_frequency(desired_frequency, vsc):
   best_frequency = 1
-  for system_clock in valid_system_clocks:
+  for system_clock in vsc:
     best_divider = round(256 * system_clock/desired_frequency) / 256
     actual_frequency = system_clock/best_divider
     if abs(actual_frequency - desired_frequency) < abs(best_frequency - desired_frequency):
@@ -42,20 +44,33 @@ def best_clock_frequency(desired_frequency):
          
 frequency = 1e6
 frequencies = []
-errors = []
+errors_new = []
+vsc = valid_system_clocks(125e6, 133e6)
 while frequency < 29e6:
-  best_frequency = best_clock_frequency(frequency*4) 
-  #print(frequency, best_frequency/4, frequency-best_frequency/4)
+  best_frequency = best_clock_frequency(frequency*4, vsc) 
   frequencies.append(frequency)
-  errors.append(frequency-best_frequency/4)
+  errors_new.append(frequency-best_frequency/4)
   frequency += 0.001e6
 
-print(max(errors))
-print(min(errors))
+frequency = 1e6
+errors_old = []
+vsc = valid_system_clocks(125e6, 125.1e6)
+while frequency < 29e6:
+  best_frequency = best_clock_frequency(frequency*4, vsc) 
+  errors_old.append(frequency-best_frequency/4)
+  frequency += 0.001e6
+
+print(max(errors_new))
+print(min(errors_new))
+print(max(errors_old))
+print(min(errors_old))
 
 from matplotlib import pyplot as plt
-plt.plot(range(len(ratios)), ratios)
-plt.show()
-plt.plot(frequencies, errors)
+plt.title("Best NCO Frequency")
+plt.xlabel("Tuned Frequency MHz")
+plt.ylabel("Distance from Nearest kHz")
+plt.plot(np.array(frequencies)/1e6, np.array(errors_old)/1e3, label="old design")
+plt.plot(np.array(frequencies)/1e6, np.array(errors_new)/1e3, label="new design")
+plt.legend()
 plt.show()
         
