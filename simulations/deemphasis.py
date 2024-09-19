421fc98b143f8df09b436f85e3b1c5eae586ff86
Mariusz Ryndzionek <mryndzionek@gmail.com>
2024-09-19 18:30:12 +0200

    Added plots to deemphasis filter design script

--- simulations/deemphasis.py
+++ simulations/deemphasis.py
@@ -1,10 +1,16 @@
 import math
 
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
 FS = 250000 // 32
 
 
 def to_fixed(arr):
     return list(map(lambda x: round(x * 32768), arr))
+
 
 def standard_deemph(tau=50e-6, fs=FS):
     w_c = 1.0 / tau
@@ -19,9 +25,28 @@ def standard_deemph(tau=50e-6, fs=FS):
     btaps = [b0 * 1.0, b0 * -z1]
     ataps = [1.0, -p1]
 
-    return to_fixed(btaps), to_fixed(ataps)
+    return btaps, ataps
+
+
+def print_fixed(b, a):
+    b, a = to_fixed(b), to_fixed(a)
+    print(f"De-emphasis 50us: bs={b}, as={a}")
+
+
+def plot_mag(title, b, a):
+    w, h = signal.freqz(b, a, fs=FS)
+    plt.plot(w, 20 * np.log10(abs(h)), "b")
+    plt.grid(True)
+    plt.xlabel("Hz")
+    plt.ylabel("dB")
+    plt.title(title)
+    plt.show()
+
 
 b, a = standard_deemph()
-print(f"De-emphasis 50us: bs={b}, as={a}")
+print_fixed(b, a)
+plot_mag("50us", b, a)
+
 b, a = standard_deemph(75e-6)
-print(f"De-emphasis 75us: bs={b}, as={a}")
+print_fixed(b, a)
+plot_mag("75us", b, a)
