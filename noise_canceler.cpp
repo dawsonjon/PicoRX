02848ad55ee8a0b3f89cc74e2f1824b3ebb66abb
Mariusz Ryndzionek <mryndzionek@gmail.com>
2025-05-03 13:06:41 +0200

    Small adjustments to the noise canceler

--- noise_canceler.cpp
+++ noise_canceler.cpp
@@ -2,15 +2,17 @@
 
 #include <string.h>
 
-#define MAG_SIZE (new_fft_size)
-#define NUM_FRAMES (4)
-#define FRAME_SWITCH_TRES \
-  (586)  // 586 corresponds to 2.5s, multiplied by NUM_FRAMES gives about 10s
-         // noise estimate
+#include "rx_definitions.h"
 
-#define ALPHA (50)       // 50.0
-#define LAMBDA (1638)    // 0.1
+#define MAG_SIZE (new_fft_size)
+#define NUM_FRAMES (10)
+
+#define ALPHA (100)      // 100.0
+#define LAMBDA (1638)    // 0.05
 #define FIX_ONE (32767)  // 1.0
+
+const uint16_t update_freq = adc_sample_rate / adc_block_size;
+const uint16_t frame_switch_tres = (update_freq);
 
 static uint16_t frames[NUM_FRAMES][MAG_SIZE];
 static uint16_t frame_idx;
@@ -30,16 +32,20 @@ void noise_canceler_init(void) {
 
 void noise_canceler_update(uint16_t mags[]) {
   for (uint16_t i = 0; i < MAG_SIZE; i++) {
-    if (mags[i] < frames[frame_idx][i]) {
-      frames[frame_idx][i] = mags[i];
-    }
+    frames[frame_idx][i] = mag_min(frames[frame_idx][i], mags[i]);
+
     // Minimum Magnitude Spectral Estimate
-    const uint32_t mmse = mag_min(mag_min(frames[0][i], frames[1][i]),
-                                  mag_min(frames[2][i], frames[3][i]));
+    uint32_t mmse = frames[0][i];
+
+    for (uint16_t j = 1; j < NUM_FRAMES; j++) {
+      mmse = mag_min(mmse, frames[j][i]);
+    }
+
     if (mags[i] > 0) {
       const uint32_t r = ((ALPHA * mmse) << 16) / mags[i];
-      if (r > (FIX_ONE - LAMBDA)) {
-        mags[i] = LAMBDA;
+      const uint32_t s = (LAMBDA * mmse) / mags[i];
+      if (r > (FIX_ONE - s)) {
+        mags[i] = s;
       } else {
         mags[i] = FIX_ONE - r;
       }
@@ -47,7 +53,7 @@ void noise_canceler_update(uint16_t mags[]) {
   }
 
   count++;
-  if (count == 586) {
+  if (count == frame_switch_tres) {
     frame_idx = (frame_idx + 1) % NUM_FRAMES;
     count = 0;
     for (uint16_t i = 0; i < MAG_SIZE; i++) {
