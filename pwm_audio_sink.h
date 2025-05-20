9101f5bf0d565aead55afb7b7af264eb765fe28d
Mariusz Ryndzionek <mryndzionek@gmail.com>
2025-05-20 22:18:38 +0200

    Another fix to the busy time measurement

--- pwm_audio_sink.h
+++ pwm_audio_sink.h
@@ -8,5 +8,6 @@
 void pwm_audio_sink_init(void);
 void pwm_audio_sink_start(void);
 void pwm_audio_sink_stop(void);
-void pwm_audio_sink_push(int16_t samples[PWM_AUDIO_NUM_SAMPLES], int16_t gain);
+uint32_t pwm_audio_sink_push(int16_t samples[PWM_AUDIO_NUM_SAMPLES],
+                             int16_t gain);
 void pwm_audio_sink_update_pwm_max(uint32_t new_max);
